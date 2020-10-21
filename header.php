5a021451955370ea66dff634e3acd2c6418f10a3
roshatron2 <roshatron2@gmail.com>
2020-10-21 17:45:24 +0530

    first commit

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Auction Site</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css" />
+    <link rel="stylesheet" href="style.css" />
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+</head>
+<nav>
+    <ul>
+        <li><a href="./index.php">Home</a></li>
+        <li><a href="sell.php">Sell</a></li>
+        <li><a href="buy.php">Buy</a></li>
+        <li><a href="items.php">Your Items</a></li>
+        <li><a href="bids.php">Your Bids</a></li>
+    </ul>
+</nav>
+<div class="container">
+
+    <body>
